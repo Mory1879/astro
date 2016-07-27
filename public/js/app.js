ab92bc545372b375d4012660a046665ef4ddbbac
Mory1879 <kirpichel@mail.ru>
2016-07-27 06:14:12 +0300

    full fixed editing and checkboxes

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,6 @@
 
 Backbone.Model.prototype.idAttribute = '_id';
 
-
-// var astronauts = [
-//   { "name": "Sigmund Jähn", "date": 272926800, "days": 7, "mission": "Sojus 31 / Sojus 29", "isMultiple": false },
-//   { "name": "Ulf Merbold", "date": 438814800, "days": 10, "mission": "STS-9", "isMultiple": true },
-//   { "name": "Reinhard Furrer", "date": 499467600, "days": 7, "mission": "STS-61-A (D1)", "isMultiple": false },
-//   { "name": "Ernst Messerschmid", "date": 499467600, "days": 7, "mission": "STS-61-A (D1)", "isMultiple": false },
-//   { "name": "Klaus-Dietrich Flade", "date": 700779600, "days": 7, "mission": "Sojus TM-14 / Sojus TM-13", "isMultiple": false },
-//   { "name": "Hans Schlegel", "date": 735768000, "days": 9, "mission": "STS-55 (D2)", "isMultiple": true },
-//   { "name": "Ulrich Walter", "date": 735768000, "days": 9, "mission": "STS-55 (D2)", "isMultiple": false },
-//   { "name": "Thomas Reiter", "date": 810072000, "days": 179, "mission": "Sojus TM-22 / Euromir 95", "isMultiple": true },
-//   { "name": "Reinhold Ewald", "date": 855522000, "days": 19, "mission": "Sojus TM-25 / Sojus TM-24", "isMultiple": false },
-//   { "name": "Gerhard Thiele", "date": 950216400, "days": 11, "mission": "STS-99", "isMultiple": false },
-//   { "name": "Alexander Gerst", "date": 1401224400, "days": 165, "mission": "Sojus TMA-13M / ISS-Expedition 40 /ISS-Expedition 41", "isMultiple": false }
-// ]
-
 // Model
 var Astronaut = Backbone.Model.extend({
   defaults: {
@@ -69,13 +54,14 @@ var AstronautView = Backbone.View.extend({
 		var date = this.$('.date').html();
 		var days = this.$('.days').html();
     var mission = this.$('.mission').html();
-    var isMultiple = this.$('.isMultiple').html();
+    var isMultiple = (this.$(".isMultiple").attr('mult'))==="true"?true:false;
+
 
 		this.$('.name').html('<input type="text" class="form-control name-update" value="' + name + '">');
 		this.$('.date').html('<input type="text" class="form-control date-update" value="' + date + '">');
 		this.$('.days').html('<input type="text" class="form-control days-update" value="' + days + '">');
     this.$('.mission').html('<input type="text" class="form-control mission-update" value="' + mission + '">');
-    this.$('.isMultiple').html('<input type="checkbox" class="form-control isMultiple-update" value="' + isMultiple + '">');
+    this.$('.isMultiple').html('<input type="checkbox" class="form-control isMultiple-update" ' + (isMultiple ? (" checked") : null) + '>');
 	},
 
   update: function() {
@@ -83,7 +69,15 @@ var AstronautView = Backbone.View.extend({
 		this.model.set('date', $('.date-update').val());
 		this.model.set('days', $('.days-update').val());
     this.model.set('mission', $('.mission-update').val());
-    this.model.set('isMultiple', $('.isMultiple-update').val());
+    this.model.set('isMultiple', $('.isMultiple-update').prop("checked") ? true : false);
+    this.model.save(null, {
+			success: function(response) {
+				console.log('Saved id: ' + response.toJSON()._id);
+			},
+			error: function() {
+				console.log('Fail!');
+			}
+		});
 	},
 
   cancel: function() {
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # Astronaut list
 Test task for RAIDIX
+
+Dependencies: Bootstrap, Express, body-parser, mongoose and MongoDB.
+
+To launch run server.js with node and open in browser http://localhost:3000/
